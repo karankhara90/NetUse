3ed21297fd1875ff1334ce51c3f240f747c089d2
karankhara <karankhara@mail.fresnostate.edu>
2015-11-09 15:27:50 -0800

    I have successfully implemented getting obj in column prevUnivName of type pointer

--- app/src/main/java/khara/karan/netuse/CreateProfile.java
+++ app/src/main/java/khara/karan/netuse/CreateProfile.java
@@ -13,6 +13,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -41,7 +44,7 @@ public class CreateProfile extends ActionBarActivity {
     int percent,gre,toefl;
     String univnameSelected, countrySelected;
     UnivDetail ud;
-    ParseQuery q2;
+
     ParseObject object;
 
 
@@ -49,16 +52,15 @@ public class CreateProfile extends ActionBarActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
-  //  try{
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         context = this;
 
 
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("UnivDetail");
-//        query.findInBackground(new FindCallback<ParseObject>() {
-//            @Override
-//            public void done(List<ParseObject> list, ParseException e) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("UnivDetail");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
 
                 // Intent intent2 = getIntent();
                 //String get_username = intent2.getStringExtra("mUsername");
@@ -93,46 +95,46 @@ public class CreateProfile extends ActionBarActivity {
 
                 list2 = new ArrayList<String>();
 
-                list2.add("--Choose University--"); list2.add("IIT Delhi"); list2.add("IIT Kharagpur");         list2.add("IIT Bombay"); list2.add("IIT Madras");
-                list2.add("IIT Guwahati"); list2.add("IIT Kanpur");  list2.add("IIT (others)");  list2.add("BITS Pilani");  list2.add("Delhi Technological University");
-                list2.add("NIT Surathkal");  list2.add("NIT Trichy");   list2.add("NIT Durgapur");      list2.add("Malaviya National Institute of Technology");
-                list2.add("NIT Rourkela");  list2.add("BIT Mesra");  list2.add("Thapar University"); list2.add("NIT Hamirpur");
-                list2.add("Jaypee Institute of Information Technology");  list2.add("PEC University"); list2.add("NIT Warangal");
-                list2.add("Indraprastha Institute of Information Technology, Delhi");  list2.add("College of Engineering, Pune");
-                list2.add("Manipal Institute of Technology");  list2.add("Pune Institute of Computer Technology");
-                list2.add("R.V. College of Engineering");   list2.add("Vellore Institute of Technology");  list2.add("Nirma Institute of Technology");
-                list2.add("UIET Panjab University");    list2.add("Anna University");  list2.add("Maharaja Agrasen Institute of Technology");
-                list2.add("Madras Institute of Technology");  list2.add("Narula Institute of Technology");  list2.add("Maharashtra Institute of Technology");
-                list2.add("Guru Tegh Bahadur Institute of Technology");  list2.add("Meerut Institute of Engineering & Technology");
-                list2.add("NIT Delhi");   list2.add("Birsa Institute of Technology");  list2.add("Chandigarh College of Engineering & Technology");
-                list2.add("Apeejay College of Engineering");   list2.add("Chitkara Institute of Engineering & Technology");
-                list2.add("Beant College of Engineering & Technology");  list2.add("IET Bhaddal Punjab");
-                list2.add("Lovely Professional University");    list2.add("Bangalore Institute of Technology");
+//                list2.add("--Choose University--"); list2.add("IIT Delhi"); list2.add("IIT Kharagpur");         list2.add("IIT Bombay"); list2.add("IIT Madras");
+//                list2.add("IIT Guwahati"); list2.add("IIT Kanpur");  list2.add("IIT (others)");  list2.add("BITS Pilani");  list2.add("Delhi Technological University");
+//                list2.add("NIT Surathkal");  list2.add("NIT Trichy");   list2.add("NIT Durgapur");      list2.add("Malaviya National Institute of Technology");
+//                list2.add("NIT Rourkela");  list2.add("BIT Mesra");  list2.add("Thapar University"); list2.add("NIT Hamirpur");
+//                list2.add("Jaypee Institute of Information Technology");  list2.add("PEC University"); list2.add("NIT Warangal");
+//                list2.add("Indraprastha Institute of Information Technology, Delhi");  list2.add("College of Engineering, Pune");
+//                list2.add("Manipal Institute of Technology");  list2.add("Pune Institute of Computer Technology");
+//                list2.add("R.V. College of Engineering");   list2.add("Vellore Institute of Technology");  list2.add("Nirma Institute of Technology");
+//                list2.add("UIET Panjab University");    list2.add("Anna University");  list2.add("Maharaja Agrasen Institute of Technology");
+//                list2.add("Madras Institute of Technology");  list2.add("Narula Institute of Technology");  list2.add("Maharashtra Institute of Technology");
+//                list2.add("Guru Tegh Bahadur Institute of Technology");  list2.add("Meerut Institute of Engineering & Technology");
+//                list2.add("NIT Delhi");   list2.add("Birsa Institute of Technology");  list2.add("Chandigarh College of Engineering & Technology");
+//                list2.add("Apeejay College of Engineering");   list2.add("Chitkara Institute of Engineering & Technology");
+//                list2.add("Beant College of Engineering & Technology");  list2.add("IET Bhaddal Punjab");
+//                list2.add("Lovely Professional University");    list2.add("Bangalore Institute of Technology");
 
 
                 univDetailHashMap = new HashMap<String, UnivDetail>();
 
-//               for (ParseObject univDetailObj : list) {
-//
-//                    univName = univDetailObj.get("univName").toString();
-//                    univDetail = new UnivDetail();
-//                    univDetail.setUnivCountry(univDetailObj.get("UnivCountry").toString());
-//                    if(univDetailObj.get("UnivCountry").toString().equals("India")){
-//                        univDetail.setUnivName(univName);
-//                        // try {
-//                        objID = univDetailObj.getObjectId().toString();
-//                        univDetail.setObjectID(objID);
-//
-//                        univDetail.setRanking(univDetailObj.get("Ranking").toString());
-//                        univRate = univDetailObj.get("univRating").toString();
-//                        univDetail.setUnivRating(univRate);
-//                        list2.add(univName);
-//
-//                        try {
-//                            univDetailHashMap.put(univName, univDetail);
-//                        } catch (Exception exp) {
-//                            Log.e("TAG", "uuniv detail HASH MAP exception: " + exp);
-//                        }
+               for (ParseObject univDetailObj : list) {
+
+                    univName = univDetailObj.get("univName").toString();
+                    univDetail = new UnivDetail();
+                    univDetail.setUnivCountry(univDetailObj.get("UnivCountry").toString());
+                    if(univDetailObj.get("UnivCountry").toString().equals("India")){
+                        univDetail.setUnivName(univName);
+                        // try {
+                        objID = univDetailObj.getObjectId().toString();
+                        univDetail.setObjectID(objID);
+
+                        univDetail.setRanking(univDetailObj.get("Ranking").toString());
+                        univRate = univDetailObj.get("univRating").toString();
+                        univDetail.setUnivRating(univRate);
+                        list2.add(univName);
+
+                        try {
+                            univDetailHashMap.put(univName, univDetail);
+                        } catch (Exception exp) {
+                            Log.e("TAG", "uuniv detail HASH MAP exception: " + exp);
+                        }
 
                         ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(context,
                                 android.R.layout.simple_spinner_item, list2);
@@ -140,8 +142,8 @@ public class CreateProfile extends ActionBarActivity {
                         dataAdapter3.notifyDataSetChanged();
                         spinner2.setAdapter(dataAdapter3);
 
-//                    }
-//                }
+                    }
+                }
 
                 mNext.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -161,15 +163,16 @@ public class CreateProfile extends ActionBarActivity {
 
                         final ParseUser newUser2 = ParseUser.getCurrentUser();
                         final ParseObject userInfo = new ParseObject("UserInfo");
-//                        q2 = ParseQuery.getQuery("UnivDetail");
-//                        q2.whereEqualTo("univName",univnameSelected);
-//                        q2.getFirstInBackground(new GetCallback() {
-//                            @Override
-//                            public void done(ParseObject parseObject, ParseException e) {
 
-//                                ud = univDetailHashMap.get(univnameSelected);
-//                                Log.e("TAG", "ud =====" + ud);
-//                                String abc= ud.getObjectID();
+                        ParseQuery<ParseObject> q2 = ParseQuery.getQuery("UnivDetail");
+                        q2.whereEqualTo("univName", univnameSelected);
+                        q2.getFirstInBackground(new GetCallback<ParseObject>() {
+                            @Override
+                            public void done(ParseObject parseObject, ParseException e) {
+
+                                //ud = univDetailHashMap.get(univnameSelected);
+                                //Log.e("TAG", "ud =====" + ud);
+                                //String abc= ud.getObjectID();
                                 //Log.e("TAG","abc))))))))))))))): "+abc);
                                 userInfo.put("userId", newUser2);
                                 //   userInfo.put("univId", newUser2);
@@ -178,37 +181,19 @@ public class CreateProfile extends ActionBarActivity {
                                 userInfo.put("greScore", gre);
                                 userInfo.put("toeflScore", toefl);
                                 userInfo.put("undergradUniv", univnameSelected);
-                               // userInfo.put("prevUnivName", parseObject);
+                                userInfo.put("prevUnivName", parseObject);
                                 userInfo.put("country", countrySelected);
                                 userInfo.put("newUnivName", "blank");
                                 userInfo.saveInBackground();
                                 //userInfo.saveInBackground(new MySaveCallBack(context));
 
-                              /*  ParseQuery<ParseObject> query = ParseQuery.getQuery("UnivDetail");
-                                query.whereEqualTo("univName", univname);
-                                query.getFirstInBackground(new GetCallback<ParseObject>() {
-                                    @Override
-                                    public void done(ParseObject parseObject, ParseException e) {
-                                        userInfo.put("prevUnivName", );
-                                     /*   ParseQuery<ParseObject> query2 = ParseQuery.getQuery("UserInfo");
-                                        query2.whereEqualTo("userId", currUser);
-                                        query2.getFirstInBackground(new GetCallback<ParseObject>() {
-                                            @Override
-                                            public void done(ParseObject parseObject, ParseException e) {
-                                                parseObject.put("prevUnivName", objId);
-                                            }
-                                        }); */
 
-                        Intent intent = new Intent(CreateProfile.this, FutureStudent.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // also clear the old one
-                        startActivity(intent);
-                    //}
-//                        @Override
-//                        public void done(Object o, Throwable throwable) {
-//
-//                        }
-                   // });
+                                Intent intent = new Intent(CreateProfile.this, FutureStudent.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // also clear the old one
+                                startActivity(intent);
+                    }
+                    });
                 //});
                               /*  Intent intent = new Intent(CreateProfile.this, FutureStudent.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -216,8 +201,8 @@ public class CreateProfile extends ActionBarActivity {
                                 startActivity(intent); */
                     }
                 });
-           // }
-       // });
+            }
+        });
 
         /*List<String> list2 = new ArrayList<String>();   list2.add("--Choose University--"); list2.add("IIT Delhi"); list2.add("IIT Kharagpur");         list2.add("IIT Bombay"); list2.add("IIT Madras");
         list2.add("IIT Guwahati"); list2.add("IIT Kanpur");  list2.add("IIT (others)");  list2.add("BITS Pilani");  list2.add("Delhi Technological University");
